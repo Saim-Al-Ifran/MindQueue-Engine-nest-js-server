cb7f5e711e9896dbd7a3d43c69452ff9c7a0e6f7
Saim Al Ifran <phpmysql811@gmail.com>
2026-01-01 21:33:34 +0600

    feat: connect application to MongoDB via Mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './user/user.module';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [UserModule, AuthModule, TasksModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI!),
+    AuthModule,
+    UsersModule,
+    TasksModule,
+  ],
 })
 export class AppModule {}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
+import { User, UserSchema } from './user.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+    ]),
+  ],
+  controllers: [UserController],
   providers: [UserService],
-  controllers: [UserController]
+  exports: [UserService],
 })
-export class UserModule {}
+export class UsersModule {}
